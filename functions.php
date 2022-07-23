ef4645e9211ffe7535576af01f8950ec05d43e32
mhadr66 <mrcranel29@gmail.com>
2022-07-23 20:03:12 +0700

    Bootstrap WordPress Theme | get_template_part

--- functions.php
+++ functions.php
@@ -7,14 +7,27 @@
  */
 
 function codecx_enqueue_scripts() {
-    wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );
-    wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
 
+    // Register Styles.
+    wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );
+    wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/library/css/bootstrap.min.css', [], false, 'all' );
+    
+    // Register Scripts.
+    wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
+    wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/library/js/bootstrap.min.js', [ 'jquery' ], false, true );
+
+    // Enqueue Style.
     wp_enqueue_style( 'style-css' );
+    wp_enqueue_style( 'bootstrap-css' );
+
+    // Enqueue Scripts.
     wp_enqueue_script( 'main-js' );
+    wp_enqueue_script( 'bootstrap-js' );
 }
 
 add_action('wp_enqueue_scripts', 'codecx_enqueue_scripts');
+
+
 
 // load script
 function load_file(){
--- header.php
+++ header.php
@@ -19,7 +19,11 @@
 </head>
 <body <?php body_class(); ?>>
 
-    <?php wp_body_open();?>
+    <?php 
+    if ( function_exists( 'wp_body_open') ) {
+        wp_body_open();
+    }
+    ?>
 
     <header>
 
