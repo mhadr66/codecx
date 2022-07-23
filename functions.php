45312e8954cb5cfcc746315f2c1e9768eb63e85d
mhadr66 <mrcranel29@gmail.com>
2022-07-23 18:55:01 +0700

    #7 enqueue scripts

--- functions.php
+++ functions.php
@@ -7,7 +7,11 @@
  */
 
 function codecx_enqueue_scripts() {
-    wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );
+    wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );
+    wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
+
+    wp_enqueue_style( 'style-css' );
+    wp_enqueue_script( 'main-js' );
 }
 
 add_action('wp_enqueue_scripts', 'codecx_enqueue_scripts');
--- header.php
+++ header.php
@@ -9,13 +9,12 @@
 <!DOCTYPE html>
 <html lang="<?php language_attributes(); ?>">
 <head>
-    <meta charset="<?php bloginfo( 'charset' ) ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
