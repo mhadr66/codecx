b8386399bdcf07b54028dcfd123dc40d37d8c3e4
mhadr66 <mrcranel29@gmail.com>
2022-07-25 22:06:33 +0700

    add_theme_support( 'post-thumbnails' ) | WordPress customizer selective

--- inc/classes/class-codecx-theme.php
+++ inc/classes/class-codecx-theme.php
@@ -46,6 +46,35 @@ class CODECX_THEME {
             'default-image'  => '',
             'default-repeat' => 'no-repeat',
         ] );
+
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support(
+            'html5', [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            ] 
+        );
+
+        add_editor_style();
+
+        add_theme_support( 'wp-block-styles' );
+
+        add_theme_support( 'align-wide' );
+
+        global $content_width;
+        if ( ! isset( $content_width ) ) {
+            $content_width = 1240;
+        }
     }
 
 }
