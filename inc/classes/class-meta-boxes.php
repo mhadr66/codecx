ba73bf4a7a69515ade8b66d7358598bc5a4aeb9c
mhadr66 <mrcranel29@gmail.com>
2022-07-30 21:59:01 +0700

    verify_nonce

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -40,6 +40,12 @@ class Meta_Boxes {
 
     public function custom_meta_box_html( $post ) {
         $value = get_post_meta( $post->ID, '_hide_page_title', true );
+
+        /**
+         * Use nonce for verification
+         */
+        wp_nonce_field( plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name' );
+
         ?>
         <label for="codecx-field"><?php esc_html_e( 'Hide the page title', 'codecx' ); ?></label>
         <select name="codecx_hide_title_field" id="codecx-field" class="postbox">
@@ -55,6 +61,24 @@ class Meta_Boxes {
     }
 
     public function save_post_meta_data( $post_id ) {
+
+        /**
+         * When the post is saved or updated we get $_POST available
+         * Check if the current use is authorized
+         */
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+
+        /**
+         * Check if the nonce value we received is the same we created.
+         */
+        if ( ! isset( $_POST['hide_title_meta_box_nonce_name'] ) ||
+            ! wp_verify_nonce( $_POST['hide_title_meta_box_nonce_name'], plugin_basename(__FILE__) )
+        ) {
+            return;
+        }
+
         if ( array_key_exists( 'codecx_hide_title_field', $_POST ) ) {
             update_post_meta(
                 $post_id,
