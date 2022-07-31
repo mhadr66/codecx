186613a19096e9e9c0d250f5f985eaafb9ab162e
mhadr66 <mrcranel29@gmail.com>
2022-07-31 23:03:40 +0700

    get_the_date

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -30,4 +30,27 @@ function the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', $add
 	echo get_the_post_custom_thumbnail( $post_id, $size, $additional_attributes );
 }
 
+function codecx_posted_on() {
+    $time_string = '<time class"entry-date published updated" datetime="%1$s">%2$s</time>';
+
+    // Post is modified
+    if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' )) {
+        $time_string = '<time class"entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf( $time_string,
+        esc_attr( get_the_date( DATE_W3C ) ),
+        esc_attr( get_the_date() ),
+        esc_attr( get_the_modified_date( DATE_W3C ) ),
+        esc_attr( get_the_modified_date() )
+    );
+
+    $posted_on = sprintf(
+        esc_attr_x( 'Posted on %s', 'post date', 'codecx' ),
+        '<a class="text-decoration-none" href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string .'</a>'
+    );
+
+    echo '<span class="posted-on text-secondary">' . $posted_on . '</span>';
+}
+
 ?>
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template for entry meta
+ * 
+ * @package Codecx
+ */
+
+?>
+
+<div class="entry-meta mb-3">
+    <?php codecx_posted_on(); ?>
+</div>
